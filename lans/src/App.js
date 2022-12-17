4257fe2ceb7b16d4b20734b970a5e9c935758b4b
07-athi <athiraazad7@gmail.com>
2022-12-17 15:47:27 +0530

    login and register page

--- lans/src/App.js
+++ lans/src/App.js
@@ -6,9 +6,13 @@ import {
 } from "react-router-dom";
 
 import './App.css';
+
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
+import Login from "./pages/login";
+import "./index.css";
+import Register from "./pages/register";
 
 const Layout = ()=>{
   return (
@@ -39,6 +43,7 @@ function App() {
   return (
     <div className="App">
         <RouterProvider router={router} />
+        
     </div>
   );
 }
