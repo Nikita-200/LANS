77f7abc97c9a04ce7991182432bfe2df6ffd99df
Sona Mathew <sonamathewc@gmail.com>
2022-12-17 19:15:29 +0530

    add sidebar

--- lans/src/pages/Home/Home.js
+++ lans/src/pages/Home/Home.js
@@ -2,13 +2,17 @@ import React from 'react'
 import HeaderBar from '../../components/HeaderBar/HeaderBar'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Posts from '../../components/Posts/Posts'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
       <HeaderBar/>
-      <Posts/>
-    <Sidebar/>
+      <div className="home">
+        <Posts/>
+        <Sidebar/>
+      </div>
+      
       
    </div>
   )
